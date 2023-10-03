eb4ebfab58f7b9d8e15fa4d6494568aea423b8ed
DevonMcfarlen <mcfarlendevon@gmail.com>
2023-10-02 21:53:06 -0500

    Add character generation functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,33 @@
 // Assignment code here
 
+function returnCapLetters(){
+  console.log(String.fromCharCode(Math.floor(Math.random() * 25) + 65));
+  return String.fromCharCode(Math.floor(Math.random() * 25) + 65);
+}
+
+function returnLowLetters(){
+  return String.fromCharCode(Math.floor(Math.random() * 25) + 97);
+}
+
+function returnNum(){
+  return Math.floor(Math.random()*9);
+}
+
+function returnSpecChars(){
+  var charCode = Math.floor(Math.random() * 93) + 33;
+
+  if(charCode > 47 && charCode < 58)
+    charCode = 58;
+  else if(charCode > 64 && charCode < 91)
+    charCode = 91;
+  else if(charCode > 96 && charCode < 123)
+    charCode = 123;
+
+  console.log(String.fromCharCode(charCode));
+
+  return String.fromCharCode(charCode);
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -9,6 +37,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  console.log(password);
   passwordText.value = password;
 
 }
